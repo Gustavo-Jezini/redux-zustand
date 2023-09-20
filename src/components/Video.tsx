cf479bbc08582968e370d460d0e439aeed7ec1dc
Gustavo-Jezini <Gustavojezini@gmail.com>
2023-09-20 10:40:20 -0300

    feat: action play video

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,13 +1,23 @@
 import ReactPlayer from 'react-player'
+import { useAppSelector } from '../store'
 
 export function Video() {
+  const lesson = useAppSelector((state) => {
+    const { currentLessonIndex, currentModuleIndex } = state.player
+
+    const lesson =
+      state.player.course.modules[currentModuleIndex].lessons[
+        currentLessonIndex
+      ]
+    return lesson
+  })
   return (
     <div className="aspect-video w-full bg-zinc-950">
       <ReactPlayer
         width="100%"
         height="100%"
         controls
-        url="https://www.youtube.com/watch?v=Jai8w6K_GnY"
+        url={`https://www.youtube.com/watch?v=${lesson.id}`}
       />
     </div>
   )
