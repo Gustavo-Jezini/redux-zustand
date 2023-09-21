3d1909523e311ff499c110555f088f5026e95f8f
Gustavo-Jezini <Gustavojezini@gmail.com>
2023-09-21 10:00:19 -0300

    feat: inicia o zustand

---
+++ src/zustand-store/index.ts
@@ -0,0 +1,65 @@
+import { create } from 'zustand'
+
+interface Course {
+  id: number
+  modules: Array<{
+    id: number
+    title: string
+    lessons: Array<{
+      id: string
+      title: string
+      duration: string
+    }>
+  }>
+}
+
+export interface PlayerState {
+  course: Course | null
+  currentLessonIndex: number
+  currentModuleIndex: number
+  isLoading: boolean
+}
+
+export const useStore = create<PlayerState>((set, get) => {
+  return {
+    course: null,
+    currentModuleIndex: 0,
+    currentLessonIndex: 0,
+    isLoading: true,
+
+    play: (moduleAndLessonIndex: [number, number]) => {
+      const [moduleIndex, lessonIndex] = moduleAndLessonIndex
+
+      set({
+        currentModuleIndex: moduleIndex,
+        currentLessonIndex: lessonIndex,
+      })
+
+      // state.currentModuleIndex = action.payload[0]
+      // state.currentLessonIndex = action.payload[1]
+    },
+
+    next: () => {
+      const { currentLessonIndex, currentModuleIndex, course } = get()
+      const nextLessonIndex = currentLessonIndex + 1
+      const nextLesson =
+        course?.modules[currentModuleIndex].lessons[nextLessonIndex]
+
+      if (nextLesson) {
+        set({
+          currentLessonIndex: nextLessonIndex,
+        })
+      } else {
+        const nextModuleIndex = currentModuleIndex + 1
+        const nextModule = course?.modules[nextModuleIndex]
+
+        if (nextModule) {
+          set({
+            currentModuleIndex: nextModuleIndex,
+            currentLessonIndex: 0,
+          })
+        }
+      }
+    },
+  }
+})
